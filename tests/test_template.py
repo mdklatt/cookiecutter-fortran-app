a7559f1661c0384c9f667848e2337453d3a271f7
Michael Klatt <mdklatt@alumni.ou.edu>
2019-06-04 21:52:21 -0500

    Refactored template test script

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,52 +1,31 @@
-""" Test the fortran-app Cookiecutter template.
+""" Test the Cookiecutter template.
 
 A template project is created in a temporary directory, the application and its
-test suite are built, and both are executed.
+test suite are built, and the test suite is executed.
 
 """
-from contextlib import contextmanager
-from json import load
-from os import chdir
-from os import getcwd
-from os.path import abspath
-from os.path import dirname
-from os.path import join
+from json import loads
+from pathlib import Path
 from shlex import split
-from shutil import rmtree
 from subprocess import check_call
-from tempfile import mkdtemp
+from tempfile import TemporaryDirectory
 
 from cookiecutter.main import cookiecutter
 
 
-def main():
+def main() -> int:
     """ Execute the test.
     
     """
-    @contextmanager
-    def tmpdir():
-        """ Enter a self-deleting temporary directory. """
-        cwd = getcwd()
-        tmp = mkdtemp()
-        try:
-            chdir(tmp)
-            yield tmp
-        finally:
-            rmtree(tmp)
-            chdir(cwd)
-        return
-
-    template = dirname(dirname(abspath(__file__)))
-    defaults = load(open(join(template, "cookiecutter.json")))
-    with tmpdir():
-        cookiecutter(template, no_input=True)
-        chdir(defaults["project_slug"])
-        check_call(split("cmake -DCMAKE_BUILD_TYPE=Debug"))
-        check_call(split("cmake --build ."))
-        main_app = join(".", defaults["project_slug"])
-        check_call(split("{:s} -h".format(main_app)))
-        # test_app = join("test", "test_{:s}".format(defaults["project_name"]))
-        # check_call(split(test_app))
+    template = Path(__file__).resolve().parents[1]
+    defaults = loads(template.joinpath("cookiecutter.json").read_text())
+    with TemporaryDirectory() as tmpdir:
+        cookiecutter(str(template), no_input=True, output_dir=tmpdir)
+        name = defaults["project_slug"]
+        cwd = Path(tmpdir) / name
+        for args in "-DCMAKE_BUILD_TYPE=Debug", "--build":
+            check_call(split(f"cmake {args:s} ."), cwd=cwd)
+        check_call(split(f"./{name:s} -h"), cwd=cwd)  # TODO: run test suite
     return 0
     
     
